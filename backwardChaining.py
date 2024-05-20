0df546b776a77cc2d28d7c57060b27a7eca12b1d
Sadnam Sakib Apurbo <sadnamsakib@iut-dhaka.edu>
2024-05-20 20:40:52 +0600

    comments

--- backwardChaining.py
+++ backwardChaining.py
@@ -6,40 +6,60 @@ class BackwardChaining:
         self.goal_stack = []
 
     def is_known(self, symbol):
-        return symbol in self.entailed_symbols
+        return (
+            symbol in self.entailed_symbols
+        )  # symbol jodi entailed_symbols e thake tahole true return korbe karon tar mane symbol ta true
 
     def has_failed(self, symbol):
-        return symbol in self.failed_symbols
+        return (
+            symbol in self.failed_symbols
+        )  # symbol jodi failed_symbols e thake tahole true return korbe karon tar mane symbol ta false
 
     def backward_chaining(self, query):
-        if self.is_known(query):
+        if self.is_known(
+            query
+        ):  # jodi query ta entailed_symbols e thake tahole true return korbe
             return True
-        if self.has_failed(query):
+        if self.has_failed(
+            query
+        ):  # jodi query ta failed_symbols e thake tahole false return korbe
             return False
-        if query in self.goal_stack:
+        if (
+            query in self.goal_stack
+        ):  # jodi query ta goal_stack e thake tahole false return korbe karon loop create hoise and symbol tar ar true howa possible na hoile etokhone hoye jaito
             return False  # Loop detected
 
-        self.goal_stack.append(query)
+        self.goal_stack.append(query)  # goal_stack e query ta append korlam
 
-        for clause in self.kb:
-            premises, conclusion = clause
+        for clause in self.kb:  # proti ta clause er upor loop chalabo
+            premises, conclusion = clause  # premise ar conclusion alada korlam
             if conclusion == query:
                 all_premises_proven = True
-                for premise in premises:
-                    if not self.backward_chaining(premise):
+                for premise in premises:  # proti ta premise er upor loop chalabo
+                    if not self.backward_chaining(
+                        premise
+                    ):  # recursively backward chaining chalabo prota premise er upor , jodi konon ekta false return kore tar mane kono ekta premise hoileo mittha hoise
                         all_premises_proven = False
                         break
-                if all_premises_proven:
-                    self.entailed_symbols.append(query)
-                    self.goal_stack.pop()
-                    return True
+                if (
+                    all_premises_proven
+                ):  # jodi shob shotti pai tahole ei conclusion ta shotti
+                    self.entailed_symbols.append(
+                        query
+                    )  # entailed_symbols e query ta append korlam
+                    self.goal_stack.pop()  # goal_stack er last element ta pop korlam
+                    return True  # true return korlam
 
-        self.failed_symbols.append(query)
-        self.goal_stack.pop()
+        self.failed_symbols.append(
+            query
+        )  # jodi kono ekta clause e shob premise shotti na pai tahole ei query ta failed_symbols e append korlam
+        self.goal_stack.pop()  # goal_stack er last element ta pop korlam
         return False
 
     def solve(self, query):
-        result = self.backward_chaining(query)
+        result = self.backward_chaining(
+            query
+        )  # backward chaining chalailam query er upor
         if result:
             print("YES: ", end=" ")
             for symbol in self.entailed_symbols:
--- forwardChaining.py
+++ forwardChaining.py
@@ -9,27 +9,47 @@ class ForwardChaining:
     def initialize(self):
         for clause in self.kb:
             if len(clause[0]) == 0:
-
-                self.agenda.append(clause[1])
+                self.agenda.append(
+                    clause[1]
+                )  # agenda list e shob conclusion gula append korsi karon amra jani je conclusion gula shotti jekhane kono premise nai , ekhane basically jeshob jinish true shegula thakbe
+                # for example jodi shudhu a,b emne deya thake etar mane a,b egula shotti
 
     def add_fact(self, fact):
-        self.facts[fact] = True
+        self.facts[fact] = (
+            True  # eta dictionary e fact gula store korar jonno,so eta track rakhbe je kon kon fact gula tru kora hoise
+        )
 
     def forward_chaining(self):
-        while self.agenda:
-            fact = self.agenda.pop(0)
-            if fact not in self.facts:
-                self.add_fact(fact)
-                self.entailed_symbols.append(fact)
-                for clause in self.kb:
-                    if fact in clause[0]:
+        while self.agenda:  # agenda khali na howa prjnto loop cholbe
+            fact = self.agenda.pop(
+                0
+            )  # ekekta agenda ke fact e nibo and agenda theke oita shoray dibo
+            if (
+                fact not in self.facts
+            ):  # jodi ei fact ta already check na hoye thake , which means self.facts dictionary te na thake
+                self.add_fact(fact)  # tokhon fact add korbo
+                self.entailed_symbols.append(
+                    fact
+                )  # fact jehetu true amra jani so entailed symbol e oita add korbo karon eta amader pore lagbe onno clause gula ke true banaite
+                for (
+                    clause
+                ) in (
+                    self.kb
+                ):  # ekhon abar prottekta clause check korbo knowledge base theke
+                    if (
+                        fact in clause[0]
+                    ):  # jodi fact ta clause[0] er moddhe thake then oitare remove korbo
                         clause[0].remove(fact)
                         if len(clause[0]) == 0:
-                            self.agenda.append(clause[1])
+                            self.agenda.append(
+                                clause[1]
+                            )  # jehetu protibar fact gulake remove kortesi ekta porjaye jokhon kono clause er shob remove hoye jabe tar mane oi conclusion ta shotti so oita agenda te push korbo
 
     def solve(self, query):
         self.forward_chaining()
-        if query in self.facts:
+        if (
+            query in self.facts
+        ):  # jodi query thake self.facts e then print yes karon query ta shotti proman hoise
             print("YES: ", end=" ")
             for symbol in self.entailed_symbols:
                 print(symbol + ",", end=" ")
